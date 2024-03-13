dddece518d721f42f6befb0fb7d5515ee3757eb4
dsajid20 <dsajid@oakland.edu>
2024-03-12 22:51:02 -0400

    assignment 2 updated

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,18 @@
-function toggleImage(imgId) {
+function showImage(imgId, btnId, hideId) {
     var img = document.getElementById(imgId);
-    if (img.style.display === "none") {
-        img.style.display = "block";
-    } else {
-        img.style.display = "none";
-    }
+    var btn = document.getElementById(btnId);
+    var hideBtn = document.getElementById(hideId);
+    img.style.display = "block";
+    btn.style.display = "none";
+    hideBtn.style.display = "block";
 }
+
+function hideImage(imgId, btnId, hideId) {
+    var img = document.getElementById(imgId);
+    var btn = document.getElementById(btnId);
+    var hideBtn = document.getElementById(hideId);
+    img.style.display = "none";
+    btn.style.display = "block";
+    hideBtn.style.display = "none";
+}
+
